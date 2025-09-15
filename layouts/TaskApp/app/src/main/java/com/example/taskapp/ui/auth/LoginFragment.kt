321dd4ced13d0314b948700b9695843a4eb4eec0
pedro.freires <pedrofreiresti@gmail.com>
2025-09-15 15:13:27 -0300

    Tradução das mensagens de erro no login

--- layouts/TaskApp/app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
+++ layouts/TaskApp/app/src/main/java/com/example/taskapp/ui/auth/LoginFragment.kt
@@ -1,6 +1,7 @@
     package com.example.taskapp.ui.auth
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -79,6 +80,8 @@ import com.google.firebase.auth.auth
                     if (task.isSuccessful) {
                         findNavController().navigate(R.id.action_global_homeFragment)
                     } else {
+                        Log.i("INFOTESTE", "loginUser: ${task.exception?.message}")
+
                         binding.progressBar.isVisible = false
                         Toast.makeText(requireContext(), task.exception?.message, Toast.LENGTH_SHORT).show()
                     }
