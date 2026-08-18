145c60b2fe5a4bdba659db936f75a19690c72c78
pedro.freires <pedrofreiresti@gmail.com>
2026-08-18 09:30:41 -0300

    feat: Configurando entidade task do db

---
+++ layouts/TaskApp/app/src/main/java/com/example/taskapp/data/db/entity/Task.kt
@@ -0,0 +1,16 @@
+package com.example.taskapp.data.db.entity
+
+import androidx.room3.Entity
+import androidx.room3.PrimaryKey
+import com.example.taskapp.data.model.Status
+
+@Entity(tableName = "word_table")
+class Task(
+    @PrimaryKey(autoGenerate = true) //Auto incremento para o id
+    val id: Long = 0,
+
+//    @ColumnInfo(name = "descrip") Se eu quisesse alterar um outro no para a coluna do db
+    val description: String,
+
+    val status: Status
+)
